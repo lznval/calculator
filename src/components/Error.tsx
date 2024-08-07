30ab63738de080136e35972e729b85a7f2e8a832
Александр Лизунов <a.lizunov@studyworld.io>
2024-08-07 11:45:54 +0400

    Init gh-pages

--- src/components/Error.tsx
+++
@@ -1,8 +0,0 @@
-import React, { FC } from "react";
-import { ErrorMessageProps } from "../types";
-
-export const Error: FC<ErrorMessageProps> = ({ message }) => {
-  return (
-    <p className="error-message">{message}</p>
-  )
-};
